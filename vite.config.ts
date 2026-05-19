f4b0702a2b26ba0ecf36b486af0bb1fec7f96337
Satyam Jha <jha876987@gmail.com>
2026-05-19 19:07:08 +0530

    Fixed asset path issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [nitro()],
-
   tanstackStart: {
     server: { entry: "server" },
+  },
+
+  vite: {
+    build: {
+      assetsDir: "assets",
+    },
   },
 });
